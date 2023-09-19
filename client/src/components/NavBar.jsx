6af0d8f1bbdd2a14cd8263625773b0f2f8fcd6a3
boone84 <boone84@Boone-PC.localdomain>
2023-09-19 15:53:33 -0400

    navbarwork

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import '../App.css';
 
+
 const Navbar = ({ userSignedIn, setUserSignedIn }) => {
     const navigate = useNavigate();
 
@@ -15,38 +16,27 @@ const Navbar = ({ userSignedIn, setUserSignedIn }) => {
         <nav className="navbar">
             <section className="navbar-menu">
                 <ul>
-                    <li>
-                        <Link to="/">Landing</Link>
-                    </li>
-                    {!userSignedIn && (
-                        <li>
-                            <Link to="/Register">Register</Link>
-                        </li>
-                    )}
+                    <li><Link to="/">Landing</Link></li>
+                    {!userSignedIn && <li><Link to="/Register">Register</Link></li>}
                     {userSignedIn && (
                         <>
-                            <li>
-                                <Link to="/Home">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/Profile">Profile</Link>
-                            </li>
-                            <li>
-                                <Link to="/PetRegisterPage">Pet Register</Link>
-                            </li>
-                            <li>
-                                <Link to="/LocalEvents">Local Events</Link>
-                            </li>
+                            <li><Link to="/Home"><img src='../photos/house-solid.png' />Home</Link></li>
+                            <li><Link to="/Profile">Profile</Link></li>
+                            <li><Link to="/PetRegisterPage">Pet Register</Link></li>
+                            <li><Link to="/LocalEvents">Local Events</Link></li>
                         </>
                     )}
-                    
                 </ul>
             </section>
             <section className="navbar-right">
                 {userSignedIn && (
-                    <button onClick={handleSignOut} className="sign-out-button">
+                    <Link 
+                        to="/" 
+                        className="sign-out-button" 
+                        onClick={handleSignOut}
+                    >
                         Sign Out
-                    </button>
+                    </Link>
                 )}
             </section>
         </nav>
@@ -54,4 +44,3 @@ const Navbar = ({ userSignedIn, setUserSignedIn }) => {
 };
 
 export default Navbar;
-
