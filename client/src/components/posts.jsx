d26da9ea391e90cbedb3afb6de3c6ce397a1149f
boone84 <boone84@Boone-PC.localdomain>
2023-09-20 16:52:53 -0400

    comments and like/dislike

--- client/src/components/posts.jsx
+++ client/src/components/posts.jsx
@@ -1,103 +1,59 @@
 import { useEffect, useState } from 'react';
 import ProfileDataFetcher from './ProfileDataFetcher';
-import Comment from './commet';
 import '../App.css';
-
-const Modal = ({ isVisible, onClose, children }) => {
-    if (!isVisible) return null;
-
-    return (
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <button onClick={onClose}>Close</button>
-                {children}
-            </div>
-        </div>
-    );
-};
 
 const Posts = () => {
     const token = localStorage.getItem('token');
-
-    // State declarations
     const [user, setUser] = useState({});
     const [posts, setPosts] = useState([]);
-    const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [postImg, setImage] = useState('');
+    const [currentCommentText, setCurrentCommentText] = useState('');
+
     const [likeCounts, setLikeCounts] = useState({});
     const [dislikeCounts, setDislikeCounts] = useState({});
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [selectedPost, setSelectedPost] = useState(null);
-    const [comments, setComments] = useState([
-        { postId: 1, commentId: 1, text: "This is a comment for post 1" }
-    ]);
+    const [comments, setComments] = useState({});
 
-    // Event Handlers
-    const handleLikeClick = (postId) => {
-        setLikeCounts((prev) => ({
-            ...prev,
-            [postId]: (prev[postId] || 0) + 1,
-        }));
-    };
-
-    const handleDislikeClick = (postId) => {
-        setDislikeCounts((prev) => ({
-            ...prev,
-            [postId]: (prev[postId] || 0) + 1,
-        }));
-    };
-
-    const handleCommentClick = (post) => {
-        setSelectedPost(post);
-        setModalVisible(true);
-    };
-
-    const handleAddComment = (postId, text) => {
-        const newComment = {
-            postId,
-            commentId: Math.random(),
-            text
-        };
-        setComments((prevComments) => [...prevComments, newComment]);
-    };
-
-    // Effects
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch('/api/posts', {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const data = await response.json();
-            setPosts(data.sort((a, b) => b.id - a.id));
+            try {
+                const response = await fetch('/api/posts', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const data = await response.json();
+                setPosts(data.sort((a, b) => b.id - a.id));
+            } catch (error) {
+                console.error("Failed to fetch posts:", error);
+            }
         };
         fetchPosts();
     }, [token]);
 
-    const addPost = async (e) => {
-        e.preventDefault();
+    const handleLike = (postId) => {
+        setLikeCounts(prev => ({
+            ...prev,
+            [postId]: (prev[postId] || 0) + 1
+        }));
+        // TODO: Update likes on the backend
+    };
 
-        const newPost = { title, content, postImg };
-        const response = await fetch('/api/posts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(newPost),
-        });
+    const handleDislike = (postId) => {
+        setDislikeCounts(prev => ({
+            ...prev,
+            [postId]: (prev[postId] || 0) + 1
+        }));
+        // TODO: Update dislikes on the backend
+    };
 
-        if (response.ok) {
-            const data = await response.json();
-            setPosts((prevPosts) => [data, ...prevPosts]);
-            setTitle('');
-            setContent('');
-            setImage('');
-        } else {
-            console.error('Failed to add a new post.');
-        }
+    const handleCommentSubmit = (postId, commentText) => {
+        setComments(prev => ({
+            ...prev,
+            [postId]: [...(prev[postId] || []), commentText]
+        }));
+        setCurrentCommentText('');  // Reset comment input
+        // TODO: Update comment in the backend
     };
 
     return (
@@ -106,42 +62,50 @@ const Posts = () => {
             <section className="post-container">
                 <h1 id="post-title">What's on your mind...</h1>
                 <section id="post-details">
-                    <form onSubmit={addPost}>
-                        <section id="post-content-photo">
-                            <textarea
-                                id="post-input-box"
-                                value={content}
-                                onChange={(e) => setContent(e.target.value)}
-                                required
-                            />
-                            <label>Add Image: </label>
-                            <input
-                                type="text"
-                                value={postImg}
-                                onChange={(e) => setImage(e.target.value)}
-                            />
-                            <button className="submit-button" type="submit">
-                                Submit
-                            </button>
-                        </section>
-                    </form>
+                    {/* TODO: Add the addPost function and form */}
                 </section>
             </section>
             <section className="feed">
-                {posts.map((post) => (
-                    <section key={post.id} className="post-box">
-                        {/* ... Rest of your post formatting code */}
-                        <Comment comments={comments.filter(c => c.postId === post.id)} />
-                        {/* ... Add a comment form or button if required */}
-                    </section>
+                {posts.map(post => (
+                    <div key={post.id} className="post-box">
+                        <img
+                            className="post-image"
+                            src={post.postImg}
+                            alt={`Post image ${post.id}`}
+                        />
+                        <h2 id="user-title">
+                            {user.firstName} {user.lastName}
+                        </h2>
+                        <p id="post-text">
+                            {post.content.length > 400
+                                ? post.content.slice(0, 400) + '...'
+                                : post.content}
+                        </p>
+                        <button onClick={() => handleLike(post.id)}>
+                            Like {likeCounts[post.id] || 0}
+                        </button>
+                        <button onClick={() => handleDislike(post.id)}>
+                            Dislike {dislikeCounts[post.id] || 0}
+                        </button>
+                        <div className="comment-section">
+                            <textarea 
+                                placeholder="Add a comment..." 
+                                value={currentCommentText}
+                                onChange={(e) => setCurrentCommentText(e.target.value)} 
+                            />
+                            <button onClick={() => handleCommentSubmit(post.id, currentCommentText)}>
+                                Comment
+                            </button>
+                        </div>
+                        {/* Display comments for the post */}
+                        <div>
+                            {comments[post.id]?.map((comment, index) => (
+                                <p key={index}>{comment}</p>
+                            ))}
+                        </div>
+                    </div>
                 ))}
             </section>
-            {isModalVisible && (
-                <Modal isVisible={isModalVisible} onClose={() => setModalVisible(false)}>
-                    {/* Render the selected post and its comments inside the modal */}
-                    {/* ... */}
-                </Modal>
-            )}
         </>
     );
 };
