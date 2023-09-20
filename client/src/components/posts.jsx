08b9001ebe8303f53f2ce9c40f47a632e80011d1
boone84 <boone84@Boone-PC.localdomain>
2023-09-19 21:28:53 -0400

    styling

--- client/src/components/posts.jsx
+++ client/src/components/posts.jsx
@@ -1,112 +1,135 @@
-import { useEffect, useState } from "react";
-import ProfileDataFetcher from "./ProfileDataFetcher";
+import { useEffect, useState } from 'react';
+import ProfileDataFetcher from './ProfileDataFetcher';
+import '../App.css';
 
 const Posts = () => {
-  const token = localStorage.getItem("token");
-  const [user, setUser] = useState({})
-  const [posts, setPosts] = useState([]);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [postImg, setImage] = useState("");
-  
-  useEffect(() => {
-    const postFetch = async () => {
-      const postsResponse = await fetch("/api/posts", {
-        headers: {
-          "Authorization": `Bearer ${token}`
-        }
-      });
-      const postsData = await postsResponse.json();
-      setPosts(postsData);
-    }
-    postFetch();
-  }, []);
-  
-  const addPost = async (e) => {
-    e.preventDefault();
+    const token = localStorage.getItem('token');
+    const [user, setUser] = useState({});
+    const [posts, setPosts] = useState([]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [postImg, setImage] = useState('');
 
-    const body = JSON.stringify({ title, content, postImg });
-    const response = await fetch('/api/posts', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      },
-      body,
-    });
-    const data = await response.json()
-    console.log(data)
+    useEffect(() => {
+        const postFetch = async () => {
+            const postsResponse = await fetch('/api/posts', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const postsData = await postsResponse.json();
+            const sortedPosts = postsData.sort((a, b) => b.id - a.id);
+            setPosts(sortedPosts);
+        };
+        postFetch();
+    }, []);
 
-    if (response.ok) {
-      
-      setTitle("");
-      setContent("");
-      setImage("");
-      
-      // Fetch the updated list of posts
-      const updatedPostsResponse = await fetch("/api/posts", {
-        headers: {
-          "Authorization": `Bearer ${token}`
+    const addPost = async (e) => {
+        e.preventDefault();
+
+        const body = JSON.stringify({ title, content, postImg });
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body,
+        });
+        const data = await response.json();
+        console.log(data);
+
+        if (response.ok) {
+            setTitle('');
+            setContent('');
+            setImage('');
+
+            // Fetch the updated list of posts
+            const updatedPostsResponse = await fetch('/api/posts', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const updatedPostsData = await updatedPostsResponse.json();
+            setPosts(updatedPostsData);
+        } else {
+            console.error('Failed to add a new post.');
         }
-      });
-      const updatedPostsData = await updatedPostsResponse.json();
-      setPosts(updatedPostsData);
-    } else {
-      console.error('Failed to add a new post.');
-    }
-  }
-  
-  const formatPosts = (post) => {
+    };
+
+    const formatPosts = (post) => {
+        return (
+            <section className="post-box">
+                <section className="img-box">
+                    {/* <h3>{post.title}</h3> */}
+                    <section>
+                        <img
+                            className="post-image"
+                            src={post.postImg}
+                            alt={`Post image ${post.id}`}
+                        />
+                    </section>
+                    <section>
+                        <section key={`Post_${post.id}`}>
+                            <h2 id="user-title">
+                                {user.firstName} {user.lastName}
+                            </h2>
+                            <section id="post-content">
+                                <p id="post-text">
+                                    {post.content.length > 400
+                                        ? post.content.slice(0, 1000) + '...'
+                                        : post.content}
+                                </p>
+                            </section>
+                        </section>
+                    </section>
+                </section>
+            </section>
+        );
+    };
+
     return (
-      <div key={`Post_${post.id}`}>
-        <h2>User: {user.firstName} {user.lastName}</h2>
-        <ul>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-          <img src={post.postImg} alt={`Post image ${post.id}`} />
-        </ul>
-      </div>
+        <>
+            <ProfileDataFetcher setUser={setUser} />
+            <section className="post-container">
+                <h1 id="post-title">What's on your mind...</h1>
+                <section id="post-details">
+                    <form onSubmit={addPost}>
+                        {/* <label>Title: </label>
+                        <input
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                        /> */}
+                        <section id="post-content-photo">
+                            {/* <label>Content: </label> */}
+                            <textarea
+                                id="post-input-box"
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                                required
+                            ></textarea>
+                            <label>Add Image: </label>
+                            <input
+                                type="text"
+                                value={postImg}
+                                onChange={(e) => setImage(e.target.value)}
+                            />
+                            <button className="submit-button" type="submit">
+                                Submit
+                            </button>
+                        </section>
+                    </form>
+                </section>
+            </section>
+            <section className="feed">
+                {posts.map((post) => {
+                    return formatPosts(post);
+                })}
+            </section>
+        </>
     );
-  };
-  
-  return (
-    <>
-    <ProfileDataFetcher setUser={setUser}/>
-      <section>
-        <h1>What's on your mind...</h1>
-        <section>
-          <p>Tell us about your pet.</p>
-          <form onSubmit={addPost}>
-            <label>Title: </label>
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-            <label>Content: </label>
-            <textarea
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              required
-            ></textarea>
-            <label>Image URL: </label>
-            <input
-              type="text"
-              value={postImg}
-              onChange={(e) => setImage(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </section>
-      </section>
-      <section>
-        {posts.map((post) => {
-          return formatPosts(post);
-        })}
-      </section>
-    </>
-  );
 };
 
 export default Posts;
