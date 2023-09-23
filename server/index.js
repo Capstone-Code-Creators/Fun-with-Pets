cc23a1242a7067da257070861cbe7da96b860052
Nathan Kim <novicenate@pop-os.localdomain>
2023-09-23 10:40:32 -0400

    deleted all /dist in server/index.js; hope this works

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   });
 
 
-app.use(express.static(path.join(__dirname, "..", "client/")));
+app.use(express.static(path.join(__dirname, "..", "client")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client/index.html"));
