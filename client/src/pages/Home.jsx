e51ac430c9936168dffcf86d3a2d71d41ae2cec2
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-09-26 13:23:01 -0500

    added Rags picture to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import ProfileDataFetcher from '../components/ProfileDataFetcher';
 import ProfileDeleteHandler from '../components/PostsDeleteHandler';
-
+import Rags from '../photos/Rags.jpeg'
 const Home = ({ token }) => {
 
 return (
@@ -9,7 +9,7 @@ return (
   <section className='homePage'>
       <h1>WELCOME TO RAGTAG!</h1>
       <p>A place in which you can connect with all things pets!</p>
-      <img src="https://media.istockphoto.com/id/1151644281/photo/little-adorable-bunny-rabbit-with-sun-glasses-stay-on-gray-table-with-brown-wood-pattern-as.jpg?s=612x612&w=0&k=20&c=--_7hZjyEu2nOsDXNnCOPsOdn6syEfIV5x0RUx5fpnc=" alt="rags" />
+      <img id="Rags" src={Rags} alt="rags" />
       <p>Our goal here at RagTag is to maintain a happy and safe environment where you can interact with others about your fluffy friends.</p>
   </section>
   
